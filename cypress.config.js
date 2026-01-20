e61b948e99fcde817203c055f55b6d089d06a981
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-01-20 23:09:40 +0530

    feat: increase page load timeout to improve test stability

--- cypress.config.js
+++ cypress.config.js
@@ -41,7 +41,7 @@ const resolveJavaCmd = () => {
 module.exports = defineConfig({
   video: false,
   defaultCommandTimeout: 5000,
-  pageLoadTimeout: 10000,
+  pageLoadTimeout: 60000,
   reporter: "mocha-allure-reporter",
   reporterOptions: {
     resultsDir: "allure-results",
